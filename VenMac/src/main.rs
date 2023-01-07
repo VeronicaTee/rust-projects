00937e338a89abbe657dbdec8f47aa1c1f120911
Veronica <veronicatomilola@gmail.com>
2023-01-07 21:21:21 +0100

    Update add money

--- VenMac/src/main.rs
+++ VenMac/src/main.rs
@@ -1,10 +1,12 @@
-fn add_money(amount: u64) -> u64 {
-    amount
-}
-
 fn main() {
 
-    let balance = add_money(300);
+    let mut balance = 0;
+
+    fn add_money(amount: u64, balance: u64) -> u64 {
+        balance + amount
+    }
+
+    balance = add_money(300, balance);
 
     println!("Available balance is: {balance}");
 }

ef54aa903f32fba09512cde9612b9957e18fc887
Veronica <veronicatomilola@gmail.com>
2023-01-08 21:38:04 +0100

    Display current balance

--- VenMac/src/main.rs
+++ VenMac/src/main.rs
@@ -1,6 +1,14 @@
+use std::io;
+
 fn main() {
 
     let mut balance = 0;
+
+    println!("Current balance is: {balance}"); 
+
+    println!("Please input the number you want to add");
+
+    
 
     fn add_money(amount: u64, balance: u64) -> u64 {
         balance + amount

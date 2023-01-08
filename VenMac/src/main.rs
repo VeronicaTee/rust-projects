cfc38a5145ac43f615534a748d77418e9f981833
Veronica <veronicatomilola@gmail.com>
2023-01-08 21:48:52 +0100

    Take user input to add

--- VenMac/src/main.rs
+++ VenMac/src/main.rs
@@ -4,11 +4,15 @@ fn main() {
 
     let mut balance = 0;
 
-    println!("Current balance is: {balance}"); 
+    println!("Welcome! Your current balance is: {balance}"); 
 
     println!("Please input the number you want to add");
 
-    
+    let mut amount = String::new();
+
+    io::stdin()
+        .read_line(&mut amount)
+        .expect("Failed to read input");
 
     fn add_money(amount: u64, balance: u64) -> u64 {
         balance + amount
